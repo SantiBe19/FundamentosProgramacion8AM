e09a72e120646d8e19e489ee05d8f04b0e29198f
SantiBe19 <santiago.bayona@upb.edu.co>
2025-08-01 09:47:03 -0500

    Variables, constantes y tipos de datos

---
+++ 1.InicioVisualStudio 1.2/1.InicioVisualStudio 1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1.InicioVisualStudio_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración e inicialización de variables
+            string nombre = "Santiago";
+            const string iva = "19%";
+            string apellidos = "Bayona Acevedo";
+            Console.Write(nombre + " bienvenido al curso de Fundamentos Programación\n");
+            nombre = "Leonardo";
+            //iva = 20%; No puedo cambiar el valor de 'iva' por ser una constante
+            Console.Write("\t" + nombre + " " + apellidos + " " + iva);
+            Console.ReadKey();
+        }
+    }
+}
