565c31e9c57cba055458a10423c056c80a59e38c
SantiBe19 <santiago.bayona@upb.edu.co>
2025-08-29 09:34:18 -0500

    Add files via upload

---
+++ 10.WHILE2/10.WHILE2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _10.WHILE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+
+            int num1;
+            int factorial = 1;
+            int i = 1;
+
+            Console.WriteLine("Ingresa un número y te lo factoreo");
+            num1 = int.Parse(Console.ReadLine());
+
+
+            while (i <= num1)
+            {
+                factorial *= i;
+                i++;
+            }
+
+            Console.WriteLine($"El factorial de este número {num1} es {factorial}");
+        }
+    }
+}
---
+++ 11.WHILE3/11.WHILE3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _11.WHILE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Realizar un algoritmo que pida números enteros positivos y los sume hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados. */
+
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("Esta es la dinámica: Vas a ingresar números positivos y yo los sumo, ingresa un número negativo si quieres parar");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                suma += numero;
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"La suma total de números ingresados es {suma}");
+        }
+    }
+}
