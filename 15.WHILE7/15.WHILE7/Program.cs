9029ffd360a256bbc27e57006d1eaf205d84655e
SantiBe19 <santiago.bayona@upb.edu.co>
2025-09-03 09:36:56 -0500

    Add files via upload

---
+++ 15.WHILE7/15.WHILE7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _15.WHILE7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo para determinar cuanto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero; además, se quiere saber cuánto lleva ahorrado cada mes.
+            int mes = 0;
+            int dineroMes;
+            int ahorrado = 0;
+
+            while (mes < 12)
+            {
+                mes++;
+                Console.WriteLine("¿Cuánto vas a meter este mes?");
+                dineroMes = int.Parse(Console.ReadLine());
+                ahorrado += dineroMes;
+            }
+            Console.WriteLine($"Lo que ahorraste tras estos doce meses fue: {ahorrado}");
+        }
+    }
+}
---
+++ 16.WHILE8/16.WHILE8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _16.WHILE8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
