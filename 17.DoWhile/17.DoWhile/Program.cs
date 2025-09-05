a5dda6b5058090a6c171e741f0d2392847625b1e
SantiBe19 <santiago.bayona@upb.edu.co>
2025-09-05 09:43:35 -0500

    Add files via upload

---
+++ 17.DoWhile/17.DoWhile/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+
+namespace _17.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 0;
+            int acumulador = 0;
+
+            do
+            {
+                contador++;
+                acumulador += contador;
+            } while (contador < 5);
+
+            Console.WriteLine($"La suma de los cinco primeros números enters es: {acumulador}");
+
+            /*Algoritmo que soliita un número y genere su correspondiente table de multiplicar desde el 1 hasta el 10, y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar.*/
+
+            /*int numeroM;
+            int i = 1;
+            int multiplo;
+            bool gen = true;
+
+
+            do
+            {
+                i = 0;
+                numeroM = 0;
+                multiplo = 0;
+                Console.WriteLine("Ey, regalame un número (entero) y te devuelvo sus múltiplos del 1 al 10 <3");
+                numeroM = int.Parse(Console.ReadLine());
+                while (i < 10)
+                {
+                    i++;
+                    multiplo = numeroM * i;
+                    Console.WriteLine($"{numeroM}*{i} = {multiplo}");
+                }
+                Console.WriteLine("¿Quieres seguir generando tablas? Y/N");
+                char solicitud;
+                solicitud = char.Parse(Console.ReadLine());
+
+                switch (solicitud)
+                {
+                    case 'Y':
+
+                        break;
+                    case 'N':
+                        gen = false;
+                        break;
+                    default:
+                        Console.WriteLine("Respusta inválida, pailas bro. Pa fuera.");
+                        gen = false;
+                        break;
+
+                }
+            }
+            while (gen);*/
+        }
+    }
+}
---
+++ 18.DoWhile2/18.DoWhile2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _18.DoWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Algoritmo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente: 
+                -Si el saldo es mayor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “Es apto para el crédito” 
+                -Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto para el crédito” 
+              El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
+              Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos. */
+            string nombre;
+            int NumCuenta;
+            int Saldo;
+            string bandera = "y";
+            int NumUsuarios = 0;
+            int SumaSaldos = 0;
+
+            do
+            {
+                NumUsuarios++;
+                Console.WriteLine("Ingresa tu nombre");
+                nombre = (Console.ReadLine());
+                Console.WriteLine("Ingresa tu número de cuenta");
+                NumCuenta = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa tu saldo");
+                Saldo = int.Parse(Console.ReadLine());
+
+                switch (Saldo)
+                {
+                    case > 3000000:
+                        Console.WriteLine($"Nombre del usuario:{nombre}, Número de cuenta {NumCuenta}, Saldo: {Saldo}");
+                        Console.WriteLine("Es apto para el crédito");
+                        SumaSaldos += Saldo;
+                        break;
+                    case < 3000000:
+                        Console.WriteLine($"Nombre del usuario:{nombre}, Número de cuenta {NumCuenta}, Saldo: {Saldo}");
+                        Console.WriteLine("No es apto para el crédito");
+                        SumaSaldos += Saldo;
+                        break;
+                }
+                Console.WriteLine("¿Desea ingresar un nuevo usuario? y/n");
+                bandera = Console.ReadLine().ToLower();
+            } while (bandera == "y");
+
+            Console.WriteLine($"La cantidad de usuarios que ingresaron sus datos fueron {NumUsuarios}");
+            Console.WriteLine($"El promedio de los saldos ingresados fue {SumaSaldos / NumUsuarios}");
+        }
+    }
+}
