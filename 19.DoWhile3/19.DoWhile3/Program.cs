d9a19e7816531960ec05999cc3ad6c84cfd399b5
SantiBe19 <santiago.bayona@upb.edu.co>
2025-09-10 09:35:37 -0500

    Add files via upload

---
+++ 19.DoWhile3/19.DoWhile3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _19.DoWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            int genero = 0;
+            int mayorEdad = 0;
+            int menorEdad = 0;
+            int hombres = 0;
+            int mujeres = 0;
+            int numeroEstudiantes = 0;
+            int cantidad = 5;
+
+            do
+            {
+                Console.WriteLine("Por favor, ingrese la edad:");
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Cuál es su género");
+                Console.WriteLine("1.Hombre 2.Mujer");
+                genero = int.Parse(Console.ReadLine());
+
+                if(edad >= 18)
+                {
+                    mayorEdad++;
+                }
+                else
+                {
+                    menorEdad++;
+                }
+
+                if (genero == 1)
+                {
+                    hombres++;
+                }
+                else
+                {
+                    mujeres++;
+                }
+              numeroEstudiantes++;
+            } while (numeroEstudiantes < cantidad);
+
+            Console.WriteLine($"De los {cantidad} estudiantes, {mayorEdad} son mayores de edad, {menorEdad} son menores de edad, {hombres} son hombres, {mujeres} son mujeres");
+        }
+    }
+}
---
+++ 20.DoWhile4/20.DoWhile4/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _20.DoWhile4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Un equipo de fútbol jugó 30 partidos en el año, se desea crear un algoritmo que permita saber: esos datos de ahí abajo
+            int partidosJugados = 0;
+            int partidosPerdidos = 0;
+            int partidosEmpatados = 0;
+            int partidosGanados = 0;
+            int cantidad = 30;
+            int pregunta;
+            int pregunta2;
+            float porcentajePP;
+            float porcentajePE;
+            float porcentajePG;
+
+            do
+            {
+                partidosJugados++;
+
+                Console.WriteLine("¿Ganaron el partido 1.Si 2.No");
+                pregunta = int.Parse(Console.ReadLine());
+                if (pregunta == 1)
+                    partidosGanados++;
+                else if (pregunta == 2)
+                {
+                    Console.WriteLine("¿Empataron? 1.Si 2.No");
+                    pregunta2 = int.Parse(Console.ReadLine());
+                    if (pregunta2 == 1)
+                        partidosEmpatados++;
+                    else
+                        partidosPerdidos++;
+                }
+            } while (partidosJugados < cantidad);
+            porcentajePP = (100 * partidosPerdidos) / cantidad;
+            porcentajePG = (100 * partidosGanados) / cantidad;
+            porcentajePE = (100 * partidosEmpatados) / cantidad;
+
+            Console.WriteLine($"El porcentaje de partidos ganados es {porcentajePG/100}, el de partidos perdidos es {porcentajePP/100} y el de partidos empatados es {porcentajePE / 100}");
+
+        }
+    }
+}
